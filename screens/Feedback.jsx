28dbe31204a1ae95570f4a84a3d6ddd4c8025655
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2023-12-24 17:00:38 +0530

    File Structure Setup

---
+++ screens/Feedback.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default function  Feedback() {
+return (
+    <View>
+    <Text> textInComponent </Text>
+    </View>
+)
+}
+
---
+++ screens/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default function home() {
+return (
+    <View>
+    <Text> textInComponent </Text>
+    </View>
+)
+}
+
