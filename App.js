940c4a32cce188a07cf7808ca2ba4ed69dbd7d58
Utsav <utsavtiwari030@outlook.com>
2023-12-27 21:14:02 +0530

    added educational content

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
+import EducationalContent from './screens/EducationalContent';
 
 export default function App() {
   return (
-<Home />
+<Home/>
   );
 }
---
+++ components/faq_education_comp/Accordion_top.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, View, Dimensions, TouchableOpacity, Image, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
+const { height } = Dimensions.get('window');
+
+export default function AccordionTop() {
+  const [fontsLoaded] = useFonts({
+    PoppinsRegular: Poppins_400Regular,
+    PoppinsBold: Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // Return null or a loading indicator while the fonts are loading
+  }
+
+  return (
+    <View style={[styles.container, { height: 0.45 * height }]}>
+      <View style={styles.safeArea}>
+        <TouchableOpacity onPress={() => console.log('Go back')}>
+          <FontAwesome name="arrow-left" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
+      <Image
+        style={styles.image}
+        source={require('../faq_education_comp/educational_content.png')} // Update the path to your image
+      />
+      <Text style={styles.text}>Education</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#00B4D8',
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+  },
+  safeArea: {
+    paddingLeft: 25,
+    paddingTop: 8,
+    marginTop: 29,
+  },
+  image: {
+    height: 140,
+    width: 220,
+    marginRight: 24,
+    justifyContent: 'center',
+    marginTop: 22,
+    alignSelf: 'flex-end',
+  },
+  text: {
+    color: 'white',
+    fontSize: 40.33,
+    fontWeight: 'bold',
+    fontFamily: 'PoppinsBold', // Use the defined font family
+    marginLeft: 38,
+    marginTop: 13,
+  },
+});
